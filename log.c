85e2df8bfb6a231fd606e0e6a05827e15d9fe6af
Dmitry Morozov-RJKV73 <dmitry.morozov@motorolasolutions.com>
2016-08-30 18:34:32 +0300

    test

---
+++ log.c
@@ -0,0 +1,45 @@
+#define GL_LOG_FILE "gl.log"
+
+int restart_gl_log() {
+	FILE* file = fopen(GL_LOG_FILE, "w");
+	if (!file) {
+	    fprintf(stderr, "ERROR: could not open GL_LOG_FILE log file %s for writing\n", GL_LOG_FILE);
+		return 0;
+	}
+	time_t now = time(NULL);
+	char* date = ctime(&now);
+	fprintf(file, "GL_LOG_FILE log. local time %s\n", date);
+	fclose(file);
+	return 1;
+}
+	
+int gl_log(const char* message, ...) {
+	va_list argptr;
+	FILE* file = fopen(GL_LOG_FILE, "a");
+	if (!file) {
+		fprintf(stderr, "ERROR: could not open GL_LOG_FILE %s file for appending\n", GL_LOG_FILE);
+		return 0;
+	}
+	va_start(argptr, message);
+	vfprintf(file, message, argptr);
+	va_end(argptr);
+	fclose(file);
+	return 1;
+}
+
+int gl_log_err (const char* message, ...) {
+    va_list argptr;
+    FILE* file = fopen(GL_LOG_FILE, "a");
+    if (!file) {
+        fprintf(stderr, "ERROR: could not open GL_LOG_FILE %s file for appending\n", GL_LOG_FILE);
+        return 0;
+    }
+    va_start(argptr, message);
+    vfprintf(file, message, argptr);
+    va_end(argptr);
+    va_start(argptr, message);
+    vfprintf(stderr, message, argptr);
+    va_end(argptr);
+    fclose(file);
+    return 1;
+}
--- main.c
+++ main.c
@@ -1,27 +1,133 @@
-#include <GL/gl.h>
-#include <GL/glu.h>
-#include "GL/glut.h"
+// http://antongerdelan.net/opengl/glcontext2.html
+// Include standard headers
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdarg.h>
 
-void display(void)
-{
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    // Here to draw
-    glutSwapBuffers();
+#define GLEW_NO_GLU 1
+#define GLFW_INCLUDE_GLCOREARB
+// Include GL headers
+#include <GL/glew.h>
+
+#include <GL/gl.h>
+#include "include/glfw3.h"
+
+#include "log.c"
+
+GLFWwindow* window;
+
+float points[] = {
+  -0.5f,  0.5f,  0.0f,
+   0.5f,  0.5f,  0.0f,
+  -0.5f, -0.5f,  0.0f,
+   0.5f, -0.5f,  0.0f,
+   0.5f,  0.5f,  0.0f,
+  -0.5f, -0.5f,  0.0f,
+};
+const char* vertex_shader =
+	"#version 400\n"
+	"in vec3 vp;"
+	"void main () {"
+	"  gl_Position = vec4 (vp.x, vp.y, vp.z, 1.0);"
+	"}";
+const char* fragment_shader =
+	"#version 400\n"
+	"out vec4 frag_colour;"
+	"void main () {"
+	"  frag_colour = vec4 (0.5, 0.0, 0.5, 1.0);"
+	"}";
+
+void glfw_error_callback(int error, const char* description) {
+    gl_log_err("GLFW ERROR: code %i msg: %s\n", error, description);
+}
+	
+int initializeGraphics(void) {
+    if (!restart_gl_log()) {
+	    fprintf( stderr, "Failed to restart GLFW log\n" );
+		return -1;
+	}
+	gl_log("Starting GLFW...\n");
+	gl_log("GLFW version: %s\n", glfwGetVersionString());
+	glfwSetErrorCallback(glfw_error_callback);
+	if(!glfwInit())
+	{
+		fprintf( stderr, "Failed to initialize GLFW\n" );
+		return -1;
+	}
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	/*glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);*/
+
+	GLFWmonitor* mon = glfwGetPrimaryMonitor ();
+	const GLFWvidmode* vmode = glfwGetVideoMode(mon);
+	GLFWwindow* window = glfwCreateWindow (vmode->width, vmode->height, "My first demo", mon, NULL);
+	
+	/*window = glfwCreateWindow(1024, 768, "Tutorial 01", NULL, NULL);
+	if(window == NULL){
+		fprintf(stderr, "Failed to open GLFW window.\n");
+		glfwTerminate();
+		return -1;
+	}*/
+	glfwMakeContextCurrent(window);
+	
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to initialize GLEW\n");
+		return -1;
+	}
+	const GLubyte* renderer = glGetString (GL_RENDERER);
+	const GLubyte* version = glGetString (GL_VERSION);
+	printf ("Renderer: %s\n", renderer);
+	printf ("OpenGL version supported %s\n", version);
+	glEnable (GL_DEPTH_TEST);
+	glDepthFunc (GL_LESS);
+	return 1;
 }
 
-void initGfx(void)
-{
-    // Here to init graphics
+int draw(void) {
+    GLuint vbo = 0;
+	glGenBuffers (1, &vbo);
+	glBindBuffer (GL_ARRAY_BUFFER, vbo);
+	glBufferData (GL_ARRAY_BUFFER, 18 * sizeof (float), points, GL_STATIC_DRAW);
+	GLuint vao = 0;
+	glGenVertexArrays (1, &vao);
+	glBindVertexArray (vao);
+	glEnableVertexAttribArray (0);
+	glBindBuffer (GL_ARRAY_BUFFER, vbo);
+	glVertexAttribPointer (0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+	GLuint vs = glCreateShader (GL_VERTEX_SHADER);
+	glShaderSource (vs, 1, &vertex_shader, NULL);
+	glCompileShader (vs);
+	GLuint fs = glCreateShader (GL_FRAGMENT_SHADER);
+	glShaderSource (fs, 1, &fragment_shader, NULL);
+	glCompileShader (fs);
+	GLuint shader_programme = glCreateProgram ();
+	glAttachShader (shader_programme, fs);
+	glAttachShader (shader_programme, vs);
+	glLinkProgram (shader_programme);
+	
+	glUseProgram (shader_programme);
+	glBindVertexArray (vao);
+	glDrawArrays (GL_TRIANGLES , 0, 6);
+	return 1;
 }
 
 int main(int argc, char **argv)
 {
-  glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-  glutInitWindowSize(800, 600);
-  glutCreateWindow("My first demo");
-  glutDisplayFunc(display);
-  initGfx();
-  glutMainLoop();
+	if (initializeGraphics() != 1) {
+	    return 0;
+	}
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+	
+	do {
+		glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		draw();
+		glfwPollEvents ();
+		glfwSwapBuffers (window);
+	}
+	while(glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0);
   return 0;
 }
