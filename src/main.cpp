2ce23a945b313826eba9cc8ac8887da0fbf4d16f
snoopdesigns <snoopdesigns@gmail.com>
2016-09-01 00:40:10 +0300

    Working plot demo

--- src/main.cpp
+++ src/main.cpp
@@ -23,14 +23,13 @@
 
 // Include GL headers
 #include <GL/gl.h>
-#define N 128
 
 GLuint program;
 GLint attribute_coord2d;
+GLint uniform_vertex_transform;
+GLint uniform_texture_transform;
 GLuint texture_id;
-GLint uniform_mvp;
-
-GLuint vbo[2];
+GLint uniform_mytexture;
 
 float offset_x = 0.0;
 float offset_y = 0.0;
@@ -38,7 +37,9 @@ float scale = 1.0;
 
 bool interpolate = false;
 bool clamp = false;
-bool rotate = true;
+bool rotate = false;
+
+GLuint vbo[2];
 
 int init_resources(void) {
 
@@ -47,12 +48,36 @@ int init_resources(void) {
 		return 0;
 
 	attribute_coord2d = get_attrib(program, "coord2d");
-	uniform_mvp = get_uniform(program, "mvp");
+	uniform_vertex_transform = get_uniform(program, "vertex_transform");
+	uniform_texture_transform = get_uniform(program, "texture_transform");
+	uniform_mytexture = get_uniform(program, "mytexture");
 
-	if (attribute_coord2d == -1 || uniform_mvp == -1)
+	if (attribute_coord2d == -1 || uniform_vertex_transform == -1 || uniform_texture_transform == -1 || uniform_mytexture == -1)
 		return 0;
 
-    // Create two vertex buffer objects
+	// Create our datapoints, store it as bytes
+#define N 256
+	GLbyte graph[N][N];
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			float x = (i - N / 2) / (N / 2.0);
+			float y = (j - N / 2) / (N / 2.0);
+			float d = hypotf(x, y) * 4.0;
+			float z = (1 - d * d) * expf(d * d / -2.0);
+
+			graph[i][j] = roundf(z * 127 + 128);
+		}
+	}
+
+	/* Upload the texture with our datapoints */
+	glActiveTexture(GL_TEXTURE0);
+	glGenTextures(1, &texture_id);
+	glBindTexture(GL_TEXTURE_2D, texture_id);
+	gl_log("Texture ID: %d",texture_id);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE, N, N, 0, GL_LUMINANCE, GL_UNSIGNED_BYTE, graph);
+
+	// Create two vertex buffer objects
 	glGenBuffers(2, vbo);
 
 	// Create an array for 101 * 101 vertices
@@ -68,7 +93,7 @@ int init_resources(void) {
 	// Tell OpenGL to copy our array to the buffer objects
 	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
 	glBufferData(GL_ARRAY_BUFFER, sizeof vertices, vertices, GL_STATIC_DRAW);
-	
+
 	// Create an array of indices into the vertex array that traces both horizontal and vertical lines
 	GLushort indices[100 * 101 * 4];
 	int i = 0;
@@ -93,25 +118,15 @@ int init_resources(void) {
 }
 
 void render(GLFWwindow* window) {
-	glClearColor(1.0, 1.0, 1.0, 1.0);
-	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 
 	glUseProgram(program);
+	glUniform1i(uniform_mytexture, 0);
 	
-	glm::mat4 model;
-	if (rotate)
-		model = glm::rotate(glm::mat4(1.0f), glm::radians(clock() / 100.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, clamp ? GL_CLAMP_TO_EDGE : GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, clamp ? GL_CLAMP_TO_EDGE : GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, interpolate ? GL_LINEAR : GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, interpolate ? GL_LINEAR : GL_NEAREST);
 
-	else
-		model = glm::mat4(1.0f);
-
-	glm::mat4 view = glm::lookAt(glm::vec3(0.0, -2.0, 2.0), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 0.0, 1.0));
-	glm::mat4 projection = glm::perspective(45.0f, 1.0f * 640 / 480, 0.1f, 10.0f);
-
-	glm::mat4 mvp_transform = projection * view * model;
-
-	glUniformMatrix4fv(uniform_mvp, 1, GL_FALSE, glm::value_ptr(mvp_transform));
-	
 	/* Draw the grid using the indices to our vertices using our vertex buffer objects */
 	glEnableVertexAttribArray(attribute_coord2d);
 
@@ -134,17 +149,76 @@ void free_resources() {
 }
 
 void logic() {
-	float angle = clock();
+	glm::mat4 model;
+
+	if (rotate) {
+		model = glm::rotate(glm::mat4(1.0f), glm::radians(clock() / 100.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+	} else {
+		model = glm::mat4(1.0f);
+	}
+
+	glm::mat4 view = glm::lookAt(glm::vec3(0.0, -2.0, 2.0), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 0.0, 1.0));
+	glm::mat4 projection = glm::perspective(45.0f, 1.0f * 640 / 480, 0.1f, 10.0f);
+
+	glm::mat4 vertex_transform = projection * view * model;
+	glm::mat4 texture_transform = glm::translate(glm::scale(glm::mat4(1.0f), glm::vec3(scale, scale, 1)), glm::vec3(offset_x, offset_y, 0));
+
+	glUniformMatrix4fv(uniform_vertex_transform, 1, GL_FALSE, glm::value_ptr(vertex_transform));
+	glUniformMatrix4fv(uniform_texture_transform, 1, GL_FALSE, glm::value_ptr(texture_transform));
 }
 
 void mainLoop(GLFWwindow* window) {
 	do {
+		glClearColor(0.8, 0.8, 1.0, 1.0);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		logic();
 		render(window);
 		glfwPollEvents();
 		glfwSwapBuffers(window);
 	} while(glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0);
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (action == GLFW_PRESS) {
+		switch (key) {
+			case GLFW_KEY_F4:
+				interpolate = !interpolate;
+				printf("Interpolation is now %s\n", interpolate ? "on" : "off");
+				break;
+			case GLFW_KEY_F2:
+				clamp = !clamp;
+				printf("Clamping is now %s\n", clamp ? "on" : "off");
+				break;
+			case GLFW_KEY_F3:
+				rotate = !rotate;
+				printf("Rotation is now %s\n", rotate ? "on" : "off");
+				break;
+			case GLFW_KEY_LEFT:
+				offset_x -= 0.03;
+				break;
+			case GLFW_KEY_RIGHT:
+				offset_x += 0.03;
+				break;
+			case GLFW_KEY_UP:
+				offset_y += 0.03;
+				break;
+			case GLFW_KEY_DOWN:
+				offset_y -= 0.03;
+				break;
+			case GLFW_KEY_PAGE_UP:
+				scale *= 1.5;
+				break;
+			case GLFW_KEY_PAGE_DOWN:
+				scale /= 1.5;
+				break;
+			case GLFW_KEY_HOME:
+				offset_x = 0.0;
+				offset_y = 0.0;
+				scale = 1.0;
+				break;	
+		}
+	}
 }
 
 int main(int argc, char **argv)
@@ -156,6 +230,7 @@ int main(int argc, char **argv)
 	if(!init_resources()) {
 	    return 0;
 	}
+	glfwSetKeyCallback(window, key_callback);
 	mainLoop(window);
 	free_resources();
   return 0;
